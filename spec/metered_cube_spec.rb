8c10b3908d13d70b4a3b9cf4663edb59481a457e
Jonathan Perrine <JonathanPerrine@Gmail.com>
2017-08-08 15:51:09 -0700

    Update metered_cube_spec.rb
    missed do

--- spec/metered_cube_spec.rb
+++ spec/metered_cube_spec.rb
@@ -15,7 +15,7 @@ describe MeteredCube do
 		end
 	end
   
-  describe "#measure_color_balance
+  describe "#measure_color_balance do
     it "returns the color delta hash" do
       @metered_cube.set_color_targets { 'White': 60, 'Blue: 60, 'Black': 60, 'Red': 60, 'Green': 60, 'Colorless' : 60}
       expect(@metered_cube.measure_color_balance.to eq({ 'White': -5, 'Blue: 10, 'Black': 0, 'Red': 20, 'Green': 0, 'Colorless' : 0})
