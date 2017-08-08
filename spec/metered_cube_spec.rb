1f677b23c118f282bdd4dde1b13d0e0c628b74aa
Jonathan Perrine <JonathanPerrine@Gmail.com>
2017-08-08 15:57:44 -0700

    Update metered_cube_spec.rb

--- spec/metered_cube_spec.rb
+++ spec/metered_cube_spec.rb
@@ -17,8 +17,8 @@ describe MeteredCube do
   
   describe "#measure_color_balance" do
     it "returns the color delta hash" do
-      @metered_cube.set_color_targets { 'White': 60, 'Blue: 60, 'Black': 60, 'Red': 60, 'Green': 60, 'Colorless' : 60}
-      expect(@metered_cube.measure_color_balance.to eq({ 'White': -5, 'Blue: 10, 'Black': 0, 'Red': 20, 'Green': 0, 'Colorless' : 0})
+      @metered_cube.set_color_targets { "White" => 60, "Blue" => 60, "Black" => 60, "Red" => 60, "Green" => 60, "Colorless" => 60}
+      expect(@metered_cube.measure_color_balance.to eq({ "White" => -5, "Blue" => 10, "Black" => 0, "Red" => 20, "Green" => 0, "Colorless" => 0})
       expect(@metered_cube.measure_color_balance_total.to eq(30)
     end
   
